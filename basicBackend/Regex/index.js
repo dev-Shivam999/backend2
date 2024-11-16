b549948958a60cdf1de1107427c4ad56d12a8cd9
dev-Shivam999 <shivamswami982@gmail.com>
2024-11-16 10:10:38 +0530

    add request

--- basicBackend/Regex/index.js
+++ basicBackend/Regex/index.js
@@ -3,6 +3,7 @@ import express from "express"
 import mongoose from "mongoose"
 
 const app = express()
+app.use(express.json())
 mongoose.connect("mongodb://localhost:27017/timepass").then(() => console.log("connect")
 ).catch(() => console.log("error",))
 const books = new mongoose.Schema({
@@ -14,6 +15,12 @@ const data = mongoose.model("books", books)
 app.get('/', async (req, res) => {
     const value = await data.find({})
     res.json({ success: true, data: value })
+})
+app.get('/body', async (req, res) => {
+    const  {id}  = req.body
+    console.log(id);
+    res.json({ success: true, id })
+
 })
 app.listen(3000, () => {
     console.log("app listening on 3000");
